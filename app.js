9f1a5beafdf11075aab328bef878c4692fd22d54
jasonxanthakis <iasonas.xanthakis@gmail.com>
2025-07-18 10:53:04 +0100

    Set up index.js, run nodemon successfully

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const cors = require('cors');
+const logger = require('./server/logger.js');
 
 const app = express();
 
 app.use(cors());
+app.use(logger);
 
 module.exports = app;
--- server/logger.js
+++ server/logger.js
@@ -1,5 +1,6 @@
 const logger = (req, res, next) => {
-    console.log(req.method, );
+    console.log(req.method, req.hostname, req.path, req.time);
+    next();
 };
 
 module.exports = logger;
